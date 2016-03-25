8da91026127e54cc41db46f2437c03033b440c1f
Kitty Tang <thepinkcodemonkey@gmail.com>
2016-03-25 16:03:35 -0700

    Added solution to grid problem

---
+++ grid/grid.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var findIt = (grid) => {
+    var gridSize = {
+        x: grid[0].length,
+        y: grid.length
+    }
+
+    //go through each row and see if we find it
+    var xLocation = null;
+    var yLocation = null;
+    for (yLocation = 0; yLocation < gridSize.y; yLocation++ ) {
+        if (xLocation !== null) {
+            break;
+        }
+        xLocation = grid[yLocation].indexOf(1) >= 0 ? grid[yLocation].indexOf(1) : null;
+    }
+
+    //recalculate yLocation from bottom
+    var yLocationFromBottom = gridSize.y - yLocation;
+    return ([xLocation, yLocationFromBottom]);
+}
+
+//location [2, 0]
+var testGrid1 = [[0, 0, 0],
+                 [0, 0, 0],
+                 [0, 0, 1]];
+
+//location[1,1]
+var testGrid2 = [[0, 0, 0, 0],
+                 [0, 1, 0, 0],
+                 [0, 0, 0, 0]];
+
+//location [3, 4];
+var testGrid3 = [[0, 0, 0, 0, 0],
+                 [0, 0, 0, 1, 0],
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0]
+             ];
+
+console.log(findIt(testGrid1));
+console.log(findIt(testGrid2));
+console.log(findIt(testGrid3));
