f0313a7bb675bd45adf107f9c8072311675b832b
Kitty Tang <thepinkcodemonkey@gmail.com>
2016-03-25 18:25:08 -0700

    Ensure params are passed on correctly
    Ensure params are passed on correctly to the method references inside
    the AClass.

--- filler/filler.js
+++ filler/filler.js
@@ -17,13 +17,17 @@ class AClass {
         console.log('Hello');
     }
 
+    repeat (words) {
+        console.log(words);
+    }
+
     /* Registers the filler function
     * @param {string} methodName - name of the method to register the filler with
     * @param {function} fillerFunction - function to be invoked before the method
     */
     before(methodName, fillerFunction) {
         //only register fillers for members that are function
-        if (typeof(this[methodName]) == 'function')  {
+        if (typeof(this[methodName]) == 'function' && methodName !=='before')  {
             if (this.fillers[methodName]) {
                 this.fillers[methodName].push(fillerFunction);
             } else {
@@ -34,10 +38,10 @@ class AClass {
             //more than once
             if (!this.funcRef[methodName]) {
                 this.funcRef[methodName] = this[methodName];
-                this[methodName] =  () => {
+                this[methodName] =  (args) => {
                     this.callfillers(methodName);
-                    this.funcRef[methodName]();
-                }
+                    this.funcRef[methodName](args);
+                };
             }
         }
     }
@@ -60,4 +64,7 @@ var a = new AClass();
 a.before('talk', function() { console.log("I'm First!")  });
 a.before('talk', function() { console.log("I'm Second!")  });
 a.before('talk', function() { console.log("I'm Third!")  });
+a.before('repeat', function() { console.log("I'm before repeat!")  });
 a.talk();
+a.repeat('woof');
+a.repeat('Polly want a cracker?');
