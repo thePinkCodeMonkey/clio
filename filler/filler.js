5bb1ce8d1109c495a86b3925639dd5a73a9c5a93
Kitty <thepinkcodemonkey@gmail.com>
2016-10-20 04:30:31 +0000

    Make use of closure for implementing hooks.

--- filler/filler.js
+++ filler/filler.js
@@ -2,16 +2,6 @@
 
 class AClass {
 
-    constructor() {
-        //storage for filler function references
-        //Takes the form of {methodName: [functionReferences]}
-        this.fillers = {};
-
-        //Storage for method references with fillers attached to them
-        //Takes the form {methodName: functionRefernce}
-        this.funcRef = {};
-    }
-
     //example method
     talk () {
         console.log('Hello');
@@ -28,43 +18,21 @@ class AClass {
     before(methodName, fillerFunction) {
         //only register fillers for members that are function
         if (typeof(this[methodName]) == 'function' && methodName !=='before')  {
-            if (this.fillers[methodName]) {
-                this.fillers[methodName].push(fillerFunction);
-            } else {
-                this.fillers[methodName] = [fillerFunction];
-            }
-
-            //overwrite the method with a new function, we don't want to do
-            //more than once
-            if (!this.funcRef[methodName]) {
-                this.funcRef[methodName] = this[methodName];
-                this[methodName] =  (args) => {
-                    this.callfillers(methodName);
-                    this.funcRef[methodName](args);
-                };
-            }
-        }
-    }
-
-    /*
-    * Checks to see if any filler is attached to a method name, invoke
-    * all fillers
-    */
-    callfillers(methodName) {
-        if (this.fillers[methodName]) {
-            this.fillers[methodName].forEach((fillerReference) => {
-                fillerReference();
-            });
+            let oldFunctionRef = this[methodName].bind(this);
+            this[methodName] = (args) => {
+                fillerFunction();
+                oldFunctionRef(args);
+            };
         }
     }
 }
 
 // simple Test
 var a = new AClass();
-a.before('talk', function() { console.log("I'm First!")  });
-a.before('talk', function() { console.log("I'm Second!")  });
-a.before('talk', function() { console.log("I'm Third!")  });
-a.before('repeat', function() { console.log("I'm before repeat!")  });
+a.before('talk', () => { console.log("I'm First!")  });
+a.before('talk', () => { console.log("I'm Second!")  });
+a.before('talk', () => { console.log("I'm Third!")  });
+a.before('repeat', () => { console.log("I'm before repeat!")  });
 a.talk();
 a.repeat('woof');
 a.repeat('Polly want a cracker?');
